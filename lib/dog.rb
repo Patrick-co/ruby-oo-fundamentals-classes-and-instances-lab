8266519a6df0eaef13c315f40985b72baab9a6b1
Patrick <patricktaylec@gmail.com>
2021-02-09 03:39:55 -0500

    commit tree 84f60c7646bea2a6d9a6fef6867effff7af5c9d8

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+#Define a class for Dog
+#Make three dogs under your Dog definition
+
+class Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+#Define a Person class
+#Make two people under your definition
+
+class Person
+end
+
+
+adele_goldberg = Person.new
+alan_kay = Person.new
